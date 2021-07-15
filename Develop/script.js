457b14df9a0b85e7cf007155157f2986ad74a748
Matt Bianco <matthewbianco@protonmail.com>
2021-07-15 19:30:31 -0400

    starter code

---
+++ Develop/script.js
@@ -0,0 +1,23 @@
+// hour box
+let time9am = $('#9amTime');
+let time10am = $('#10amTime');
+let time11am = $('#11amTime');
+let time12am = $('#12amTime');
+let time1pm = $('#1pmTime');
+let time2pm = $('#2pmTime');
+let time3pm = $('#3pmTime');
+let time4pm = $('#4pmTime');
+let time5pm = $('#5pmTime');
+//hour box end
+
+// text area
+let text9am = $("#9amText");
+let text10am = $("#10amText");
+let text11am = $("#11amText");
+let text12am = $("#12amText");
+let text1pm = $("#1pmText");
+let text2pm = $("#2pmText");
+let text3pm = $("#3pmText");
+let text4pm = $("#4pmText");
+let text5pm = $("#5pmText");
+// text area end
